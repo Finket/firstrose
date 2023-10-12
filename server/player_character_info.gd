152d8b79f3ab6073b0af580d0ce37655f985ea90
Finket <va.ketola@gmail.com>
2023-10-12 08:28:58 +0300

    Update dedicated server synchronizers

--- server/player_character_info.gd
+++ server/player_character_info.gd
@@ -1,14 +1,4 @@
 extends Node
 
 func _ready():
-	name = str(get_multiplayer_authority())
-
-
-@rpc("unreliable")
-func remote_set_position(authority_position): pass
-
-@rpc("authority", "call_local", "reliable", 1)
-func display_message(message): pass
-
-@rpc("any_peer", "call_local", "reliable", 1)
-func clicked_by_player(): pass
+	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
