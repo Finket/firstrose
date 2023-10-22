14c69f4a37cd3e073b3e4fc3553a153c3660d129
Finket <va.ketola@gmail.com>
2023-10-22 06:03:57 +0300

    Test chat networking options

--- client/scripts/TextEdit.gd
+++ client/scripts/TextEdit.gd
@@ -2,10 +2,12 @@ extends TextEdit
 
 func _on_text_changed():
 	var sync = $Sync
-	if is_multiplayer_authority():
-		sync.text = text
+	sync.text = text
+	#if is_multiplayer_authority():
+	#	sync.text = text
 
 func _process(_delta):
 	var sync = $Sync
-	if not is_multiplayer_authority():
-		text = sync.text
+	text = sync.text
+	#if not is_multiplayer_authority():
+	#	text = sync.text
